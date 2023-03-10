ffd522f8f984c44d4817b2aac3f76adc291d4337
Evgenygroup <mailfrom61@gmail.com>
2023-03-10 17:28:05 +0100

    adds consultation 10.03

---
+++ consultations/2023-03-10/code/consultation_2023_03_10/src/Person.java
@@ -0,0 +1,23 @@
+public class Person {
+
+    String name;
+    int age;
+
+    public Person(String name,int age){
+        this.name = name;
+        this.age = age;
+    }
+
+    void canWalk(){
+        System.out.println("A man can walk");
+    }
+
+    void canSpeak(){
+        System.out.println("I can speak");
+    }
+
+
+
+
+
+}
